bce72ac00199d0f170c05ff6d5de9b93bc8096ab
Ferrin <fthreatt19@gmail.com>
2021-08-03 23:35:59 -0400

    fixed token

--- static/script.js
+++ static/script.js
@@ -19,7 +19,26 @@
 //         const data = await result.json();
 //         return data.access_token;
 //     }
-
+// const APIController = (function() {
+//
+//     const clientId = 'ADD YOUR CLIENT ID';
+//     const clientSecret = 'ADD YOUR CLIENT SECRET';
+//
+//     // private methods
+//     const _getToken = async () => {
+//
+//         const result = await fetch('https://accounts.spotify.com/api/token', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type' : 'application/x-www-form-urlencoded',
+//                 'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+//             },
+//             body: 'grant_type=client_credentials'
+//         });
+//
+//         const data = await result.json();
+//         return data.access_token;
+//     }
 
 const _getToken = async () => {
 
